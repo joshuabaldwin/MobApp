00416d5682b5fac376709c753a3a0b775f62ccde
Joshua Baldwin <jbaldwi6@asu.edu>
2014-11-12 21:19:35 -0700

    Had to add backdoor to get around parse issue

--- TextbookExchange/ViewController.m
+++ TextbookExchange/ViewController.m
@@ -27,9 +27,9 @@
     //[self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"Textbooks"]]];
     
     /*PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
-    testObject[@"foo"] = @"bar";
-    [testObject saveInBackground];*/
-
+     testObject[@"foo"] = @"bar";
+     [testObject saveInBackground];*/
+    
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
@@ -44,34 +44,44 @@
 }
 
 /*
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    AccountInfoViewController *viewController1 = segue.destinationViewController;
-    EditUserInfoViewController *viewController2 = segue.destinationViewController;
-    viewController1.username = self.username.text;
-    viewController2.username = self.username.text;
-    
-    [PFUser currentUser];
-}
-*/
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+ {
+ AccountInfoViewController *viewController1 = segue.destinationViewController;
+ EditUserInfoViewController *viewController2 = segue.destinationViewController;
+ viewController1.username = self.username.text;
+ viewController2.username = self.username.text;
+ 
+ [PFUser currentUser];
+ }
+ */
 
-- (IBAction)logIn:(id)sender {
-    [PFUser logInWithUsernameInBackground:_username.text password:_passcode.text block:^(PFUser *user, NSError *error) {
-        if (user)
-        {
-            // Do stuff after successful login.
-            //AccountInfoViewController *viewController = [[AccountInfoViewController alloc] init];
-            
-            //[self.navigationController pushViewController:viewController animated:YES];
-            [self performSegueWithIdentifier:@"AccountInfo" sender:self];
-        }
-        else
-        {
-            // The login failed. Check error to see why.
-            self.alert = [[UIAlertView alloc] initWithTitle:@"Invalid Log-in!" message:@"Incorrect username or passcode." delegate:self cancelButtonTitle:@"Back" otherButtonTitles:nil];
-            [self.alert show];
-        }
-    }];
+- (IBAction)logIn:(id)sender
+{
+    // Joshua: Added login backdoor for testing and development
+    if (([self.username.text caseInsensitiveCompare:@"JSB"] == NSOrderedSame) &&
+        ([self.passcode.text caseInsensitiveCompare:@"JSB"] == NSOrderedSame))
+    {
+        [self performSegueWithIdentifier:@"AccountInfo" sender:self];
+    }
+    else
+    {
+        [PFUser logInWithUsernameInBackground:_username.text password:_passcode.text block:^(PFUser *user, NSError *error) {
+            if (user)
+            {
+                // Do stuff after successful login.
+                //AccountInfoViewController *viewController = [[AccountInfoViewController alloc] init];
+                
+                //[self.navigationController pushViewController:viewController animated:YES];
+                [self performSegueWithIdentifier:@"AccountInfo" sender:self];
+            }
+            else
+            {
+                // The login failed. Check error to see why.
+                self.alert = [[UIAlertView alloc] initWithTitle:@"Invalid Log-in!" message:@"Incorrect username or passcode." delegate:self cancelButtonTitle:@"Back" otherButtonTitles:nil];
+                [self.alert show];
+            }
+        }];
+    }
 }
 
 
